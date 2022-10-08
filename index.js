0ab67ef4ad26b7e88530002ef211018475fb03c2
Stephen Weaver <sweaver1@ND.EDU>
2022-10-07 23:18:41 -0400

    Update index.js
    require express
    
    use GET route to locate /movies
    
    use express.static to serve documenation.html
    
    middleware error log

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const express = require('express');
+const { application } = require('express');
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path');
+
 const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my awesome movies app!');
+});
+
+app.get('/movies', (req, res) => {
+  res.send('My Top 10 Movies!');
+});
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('What did you do!?');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
