514b966cc76cdf0435d3bcb039c521529b8c5749
Stephen Weaver <sweaver1@ND.EDU>
2022-10-19 13:38:52 -0400

    Create models.js

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  year: { type: String, required: true },
+  rating: { type: String, required: true },
+  description: { type: String, required, true},
+  genre: {
+    name: String,
+    description: String
+  },
+  director: {
+    name: String,
+    bio: String,
+    birth: Date
+  },
+  actors: [String],
+  imagePath: String,
+  featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birth: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
