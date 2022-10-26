4266780147d566ef87d40c2da44c08e4ae23e51a
Stephen Weaver <sweaver1@ND.EDU>
2022-10-26 14:44:25 -0400

    Update models.js
    hashPassword function to hash submitted passwords
    
    validatePassword  to compare submitted hashed passwords with the hashed passwords stored in the DB

--- models.js
+++ models.js
@@ -19,13 +19,21 @@ let movieSchema = mongoose.Schema({
   featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
+let userSchema = mongose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
   birth: Date,
   favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statistics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
