95e8f0144f33e265eb1b27b72756e93dfaceded6
Stephen Weaver <sweaver1@ND.EDU>
2022-11-02 10:03:55 -0400

    Update index.js
    can I update email?

--- index.js
+++ index.js
@@ -178,7 +178,8 @@ app.put('/users/:username', passport.authenticate('jwt', { session: false }),
   [
     check('username', 'Username is required').isLength({ min: 5 }),
     check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('password', 'Password is required').not().isEmpty()
+    check('password', 'Password is required').not().isEmpty(),
+    check('email', 'Email does not appear to be valid').isEmail()
   ], (req, res) => {
 
     let errors = validationResult(req);
