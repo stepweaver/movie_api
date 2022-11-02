b87d31371c8f9b38da9050a0bf793bfe3d751fe1
Stephen Weaver <sweaver1@ND.EDU>
2022-11-02 09:44:18 -0400

    Update index.js
    update users/:username

--- index.js
+++ index.js
@@ -128,16 +128,17 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
     });
 });
 
-// app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
-//   User.findOne({ username: req.params.username })
-//     .then((user) => {
-//       res.status(200).json(user);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(404).send('User not found.');
-//     });
-// });
+app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  User.findOne({ username: req.params.username })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send("Dave's not here! User not found.");
+      } else {
+        res.status(200).json(user);
+      }
+      res.status(200).json(user);
+    });
+  });
 
 app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movie.findOne({ title: req.params.title })
