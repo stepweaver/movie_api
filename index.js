dc9f7bf2b82fd2aa5e4fe0ae9f55ab992352ec00
Stephen Weaver <sweaver1@ND.EDU>
2022-11-02 09:23:09 -0400

    Update index.js
    update error catch

--- index.js
+++ index.js
@@ -162,13 +162,13 @@ app.get('/movies/genre/:name', passport.authenticate('jwt', { session: false }),
 });
 
 app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movie.findOne({ 'movie.director': req.params.director })
+  Movie.findOne({ 'director.name': req.params.name })
     .then((movie) => {
-      res.status(200).json(movie.director);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(404).send('Director not found.');
+      if (!movie) {
+        res.status(404).send("Dave's not here! Director not found.");
+      } else {
+        res.status(200).json(movie.director);
+      }
     });
 });
 
