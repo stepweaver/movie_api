b94c4837da5eecbf19573e55ab20c8c433ae1818
Stephen Weaver <sweaver1@ND.EDU>
2022-11-16 21:13:11 -0500

    Update index.js
    CORS allow localhost:1234

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const express = require('express'),
   
   const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
   
-  let allowedOrigins = ['http://localhost:1234', 'http://cthulhu8080.herkouapp.com']
+  let allowedOrigins = ['http://localhost:8080', 'http://cthulhu8080.herkouapp.com', 'http://localhost:1234']
   app.use(cors({
     origin: (origin, callback) => {
     if (!origin) return callback(null, true);
